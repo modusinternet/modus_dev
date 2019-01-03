83e868e182d0551e6236e957f79570ea23fa23d6
Vincent Hallberg <vince@modusinternet.com>
2019-01-03 15:45:42 -0800

    asdf

--- ccmstpl/_js/sw_cached_site.js
+++ ccmstpl/_js/sw_cached_site.js
@@ -8,14 +8,11 @@ self.addEventListener('install',e=>{
 // Call Activate Event
 self.addEventListener('activate',e=>{
 	console.log('Service Worker: Activated');
-	// Remove unwanted caches
+	// clear old cache
 	e.waitUntil(
-		caches
-		.keys()
-		.then(cacheNames=>{
+		caches.keys().then(cacheNames=>{
 			return Promise.all(
-				cacheNames
-				.map(cache=>{
+				cacheNames.map(cache=>{
 					if(cache!==cacheName) {
 						console.log('Service Worker: Clearing Old Cache');
 						return caches.delete(cache);
@@ -31,19 +28,15 @@ self.addEventListener('activate',e=>{
 self.addEventListener('fetch',e=>{
 	console.log('Service Worker: Fetching');
 	e.respondWith(
-		fetch(e.request)
-		.then(res=>{
+		fetch(e.request).then(res=>{
 			// Make clone of response
 			const resClone=res.clone();
 			// Open cahce
-			caches
-			.open(cacheName)
-			.then(cache=>{
+			caches.open(cacheName).then(cache=>{
 				// Add response to cache
 				cache.put(e.request,resClone);
 			});
 			return res;
-		})
-		.catch(err=>caches.match(e.request).then(res=>res))
+		}).catch(err=>caches.match(e.request).then(res=>res))
 	);
 });
