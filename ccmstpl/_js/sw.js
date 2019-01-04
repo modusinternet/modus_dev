a7bb9672cebbc0cf11bda360ffbff934f15cb753
Vincent Hallberg <vince@modusinternet.com>
2019-01-04 03:00:52 -0800

    asdf

--- ccmstpl/_js/sw.js
+++ ccmstpl/_js/sw.js
@@ -3,10 +3,16 @@ if(navigator.serviceWorker){
 	/* You could use a function call like function() but we us ES6 arrow functions below to reduce syntax, () => {} */
 	window.addEventListener('load', () => {
 		navigator.serviceWorker
-		.register('/ccmstpl/_js/sw_cached_site.js')
+		.register('./sw_cached_site.js')
 		.then(reg => console.log('Service Worker: Registered (Site)'))
 		.catch(err => console.log(`Service Worker: Error: ${err}`));
 	});
 } else {
 		console.log('Service Worker not supported.');
 }
+
+/*
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker2.js');
+}
+*/
