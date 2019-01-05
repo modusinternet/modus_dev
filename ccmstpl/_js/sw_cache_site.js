d8b9946840ffbe8448827d2f8b692177a6ee3238
Vincent Hallberg <vince@modusinternet.com>
2019-01-05 02:23:20 -0800

    asdf

--- ccmstpl/_js/sw_cache_site.js
+++ ccmstpl/_js/sw_cache_site.js
@@ -49,6 +49,7 @@ self.addEventListener('install', e => {
 	console.log('Service Worker: Installed');
 });
 
+
 /*
 self.addEventListener('activate', e => {
 	console.log('Service Worker: Activated');
@@ -68,32 +69,51 @@ self.addEventListener('activate', e => {
 });
 */
 self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-		cacheNames.filter(function(cacheName) {
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					// Return true if you want to remove this cache,
+					// but remember that caches are shared across
+					// the whole origin
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
 });
 
 
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(cacheName).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
+self.addEventListener('fetch', e => {
+	console.log('Service Worker: Fetching');
+	e.respondWith(
+		fetch(e.request).then(res => {
+			// Make clone of response
+			const resClone = res.clone();
+			// Open cahce
+			caches.open(cacheName).then(cache => {
+				// Add response to cache
+				cache.put(e.request, resClone);
+			});
+			return res;
+		}).catch(err => caches.match(e.request).then(res => res))
+	);
 });
+
+/*
+self.addEventListener('fetch', e => {
+	console.log('Service Worker: Fetching');
+	e.respondWith(
+		caches.open(cacheName).then(cache => {
+			return cache.match(e.request).then(response => {
+				return response || fetch(e.request).then(response => {
+					cache.put(e.request, response.clone());
+					return response;
+				});
+			});
+		})
+	);
+});
+*/
