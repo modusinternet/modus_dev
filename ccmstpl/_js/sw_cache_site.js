be6af9a86e0c3bb1bdd545efdd936002880ed4a0
Vincent Hallberg <vince@modusinternet.com>
2019-01-05 12:14:25 -0800

    asdf

--- ccmstpl/_js/sw_cache_site.js
+++ ccmstpl/_js/sw_cache_site.js
@@ -168,8 +168,45 @@ self.addEventListener('fetch', function(event) {
 	);
 });
 */
+/*
 self.addEventListener('fetch', function(e) {
 	console.log('[ServiceWorker] Fetch', e.request.url);
+	// e.respondWidth Responds to the fetch event
+	e.respondWith(
+		// Check in cache for the request being made
+		caches.match(e.request).then(function(response) {
+			// If the request is in the cache
+			if (response) {
+				console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+				// Return the cached version
+				return response;
+			}
+			// If the request is NOT in the cache, fetch and cache
+			var requestClone = e.request.clone();
+			return fetch(requestClone).then(function(response) {
+				if ( !response ) {
+					console.log("[ServiceWorker] No response from fetch ")
+					return response;
+				}
+				var responseClone = response.clone();
+				// Open the cache
+				caches.open(cacheName).then(function(cache) {
+					// Put the fetched response in the cache
+					cache.put(e.request, responseClone);
+					console.log('[ServiceWorker] New Data Cached', e.request.url);
+					// Return the response
+					return response;
+				}); // end caches.open
+			}).catch(function(err) {
+				console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+			});
+		}) // end caches.match(e.request)
+	); // end e.respondWith
+});
+*/
+self.addEventListener('fetch', function(e) {
+	//console.log('[ServiceWorker] Fetch', e.request.url);
+	console.log(`[ServiceWorker] Fetch: ${e.request.url}`);
 	// e.respondWidth Responds to the fetch event
 	e.respondWith(
 		// Check in cache for the request being made
