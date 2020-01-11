af2d71be6d971ffadd68a3aad3122527147f9e71
Vince <vince@modusinternet.com>
2020-01-10 19:37:48 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -22,30 +22,6 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 		</script>
 	</head>
 	<body>
-		<!-- div id="wrapper">
-			{  CCMS_TPL:header-body.php}
-
-
-			<div id="page-wrapper">
-				<div class="row">
-					<div class="col-md-12">
-						<h1 class="page-header">Dashboard</h1>
-						<div class="panel panel-danger">
-							<div class="panel-heading">
-								Notice
-							</div>
-							<div class="panel-body">
-								<p>This section of the Custodian CMS admin is currently under development.</p>
-							</div>
-						</div>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec ligula id nisl fringilla finibus. Vestibulum rhoncus, felis at fringilla ullamcorper, ante mi tincidunt nunc, ac ultrices odio odio vitae lorem. Morbi quis elit id urna efficitur aliquam ut et sapien. Fusce porttitor vel ligula faucibus tempor. Pellentesque tincidunt imperdiet enim, id lobortis ipsum tempus id. In facilisis elementum dictum. Donec suscipit ornare tortor, sed volutpat mauris volutpat at. Pellentesque porttitor ut augue at ultrices. Proin egestas semper lorem quis suscipit. Vivamus eget magna tincidunt, semper sem eu, molestie quam. Praesent nisl velit, ultricies ac malesuada id, dapibus in dui. Mauris luctus velit non mi condimentum rhoncus. Nullam sit amet aliquet turpis, id malesuada nulla. Ut sit amet nisl nec ante commodo eleifend.
-
-
-					</div>
-				</div>
-			</div>
-		</div -->
-
 		<nav id="menu">
 			<ul>
 				<li><a href="/">Home</a></li>
@@ -77,34 +53,9 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 		<script>
 			function loadFirst(e,t){var a=document.createElement("script");a.async = true;a.readyState?a.onreadystatechange=function(){("loaded"==a.readyState||"complete"==a.readyState)&&(a.onreadystatechange=null,t())}:a.onload=function(){t()},a.src=e,document.body.appendChild(a)}
 
-			var cb = function() {
-				//var l = document.createElement('link'); l.rel = 'stylesheet';
-				//l.href = "/ccmsusr/_css/bootstrap-3.3.7.min.css";
-				//var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-
-				//var l = document.createElement('link'); l.rel = 'stylesheet';
-				//l.href = "/ccmsusr/_css/metisMenu-2.4.0.min.css";
-				//var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-
-				//var l = document.createElement('link'); l.rel = 'stylesheet';
-				//l.href = "/ccmsusr/_css/custodiancms.css";
-				/*l.href = "/ccmsusr/_css/custodiancms.min.css";*/
-				//var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-
-				//var l = document.createElement('link'); l.rel = 'stylesheet';
-				//l.href = "/ccmsusr/_css/font-awesome-4.7.0.min.css";
-				//var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-
-				var l = document.createElement('link'); l.rel = 'stylesheet';
-				l.href = "/ccmsusr/_css/mmenu.css";
-				var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-
-				//path/to/js/mmenu.js
-			};
-
-			var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
-			if (raf) raf(cb);
-			else window.addEventListener('load', cb);
+			var l = document.createElement('link'); l.rel = 'stylesheet';
+			l.href = "/ccmsusr/_css/mmenu.css";
+			var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
 
 			function loadJSResources() {
 				loadFirst("/ccmsusr/_js/jquery-3.4.1.min.js", function() { /* JQuery is loaded */
