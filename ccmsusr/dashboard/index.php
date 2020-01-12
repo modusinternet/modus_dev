ce941d429c4c31944f6b52fa3a435af56d39819f
Vince <vince@modusinternet.com>
2020-01-11 18:05:00 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -45,7 +45,22 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 		<script>
 			document.addEventListener(
 				"DOMContentLoaded", () => {
-					new Mmenu( "#my-menu" );
+					new Mmenu( "#my-menu"", {
+						"extensions": [
+							"pagedim-black",
+							"position-right"
+						], "iconbar": {
+							"use": true,
+							"top": [
+								"<a href='#/'><i class='fa fa-home'></i></a>",
+								"<a href='#/'><i class='fa fa-user'></i></a>"
+							], "bottom": [
+								"<a href='#/'><i class='fa fa-twitter'></i></a>",
+								"<a href='#/'><i class='fa fa-facebook'></i></a>",
+								"<a href='#/'><i class='fa fa-linkedin'></i></a>"
+							]
+						}
+					});
 				}
 			);
 		</script>
