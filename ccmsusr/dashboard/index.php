068d76b8d15fcfc0d21cc03ad6d3222f3111852c
Vince <vince@modusinternet.com>
2020-02-12 09:30:22 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -182,14 +182,58 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 			/* 1367px or larger. */
 			@media only screen and (min-width:1367px){}
 
+
+
+
+
+
+
+
+			.panel {
+				margin-bottom: 20px;
+				background-color:
+				#fff;
+				border: 1px solid transparent;
+				border-top-color: transparent;
+				border-right-color: transparent;
+				border-bottom-color: transparent;
+				border-left-color: transparent;
+				border-radius: 4px;
+				-webkit-box-shadow: 0 1px 1px rgba(0,0,0,.05);
+				box-shadow: 0 1px 1px
+				rgba(0,0,0,.05);
+			}
+
+			.panel-danger{border-color:#ebccd1}
+
+			.panel-heading {
+			    padding: 10px 15px;
+			    border-bottom: 1px solid
+			    transparent;
+			        border-bottom-color: transparent;
+			    border-top-left-radius: 3px;
+			    border-top-right-radius: 3px;
+			}
+
+			.panel-danger > .panel-heading {
+    color:
+#a94442;
+background-color:
+#f2dede;
+border-color:
+    #ebccd1;
+}
+
+.panel-body {
+    padding: 15px;
+ }
+
 		</style>
 	</head>
 	<body>
 		<div id="my-page">
 			<div id="my-header">
-				<a href="#my-menu"><svg aria-label="title desc" class="svg-nav-icon" role="img" viewBox="0 -53 384 384" xmlns="https://www.w3.org/2000/svg"><title>Open Menu</title><desc>An icon used to open the navigation menu.</desc><a xlink:href="#"><path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/></a></svg></a>
-
-				<a href="#my-menu"><svg aria-label="title desc" class="svg-nav-icon" role="img" viewBox="0 -53 384 384" xmlns="https://www.w3.org/2000/svg"><title>Open Menu</title><desc>An icon used to open the navigation menu.</desc><path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/></svg></a>
+				<a href="my-menu"><svg aria-label="title desc" class="svg-nav-icon" role="img" viewBox="0 -53 384 384" xmlns="https://www.w3.org/2000/svg"><title>Open Menu</title><desc>An icon used to open the navigation menu.</desc><path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/></svg></a>
 				<nav id="my-menu">
 					<ul>
 						<li><a href="/en/user/dashboard/">Dashboard</a></li>
@@ -208,13 +252,38 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 			</div>
 
 			<div id="my-content">
-				...
+
+
+
+
+
+
+
+
+
+
+				<h1>Content Manager</h1>
+				<div class="panel panel-danger">
+					<div class="panel-heading">
+						Notice
+					</div>
+					<div class="panel-body">
+						<p>This section of the Custodian CMS admin is currently under development.</p>
+					</div>
+				</div>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec ligula id nisl fringilla finibus. Vestibulum rhoncus, felis at fringilla ullamcorper, ante mi tincidunt nunc, ac ultrices odio odio vitae lorem. Morbi quis elit id urna efficitur aliquam ut et sapien. Fusce porttitor vel ligula faucibus tempor. Pellentesque tincidunt imperdiet enim, id lobortis ipsum tempus id. In facilisis elementum dictum. Donec suscipit ornare tortor, sed volutpat mauris volutpat at. Pellentesque porttitor ut augue at ultrices. Proin egestas semper lorem quis suscipit. Vivamus eget magna tincidunt, semper sem eu, molestie quam. Praesent nisl velit, ultricies ac malesuada id, dapibus in dui. Mauris luctus velit non mi condimentum rhoncus. Nullam sit amet aliquet turpis, id malesuada nulla. Ut sit amet nisl nec ante commodo eleifend.
+
+
+
+
+
+
 			</div>
 		</div>
 		<script src="/ccmsusr/_js/mmenu.js"></script>
 		<script>
 			document.addEventListener("DOMContentLoaded", () => {
-				new Mmenu( "#my-menu", {
+				new Mmenu("#my-menu", {
 					"extensions": [
 						"pagedim-black",
 						"position-left"
